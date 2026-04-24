36a332cb9a6fa8c029ca90d999fbe25021be9dc6
BT_C09_20 <mobileappdev@bths.edu>
2026-04-24 13:28:51 -0400

    Initial Commit

---
+++ collabdemo_Yuen/collabdemo_YuenApp.swift
@@ -0,0 +1,17 @@
+//
+//  collabdemo_YuenApp.swift
+//  collabdemo_Yuen
+//
+//  Created by Student on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct collabdemo_YuenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
